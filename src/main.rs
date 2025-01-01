30ccbd054021d374d04f3f47a449fa1be85cad5c
TwilCynder <twilcynder@gmail.com>
2025-01-01 17:42:46 +0100

    base module org + errors system

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,25 @@
 use std::fs;
 
-use yaml_rust2::YamlLoader;
+use yaml_rust2::{yaml::Hash, Yaml, YamlLoader};
+use config::Config;
+
+mod config;
+mod errors;
+
+fn handle_config(data: &Yaml){
+    
+}
 
 fn main() {
-    let res = fs::read_to_string("./test/test.yaml").expect("Couldn't read file");
-    let doc = YamlLoader::load_from_str(&res);
+    let raw_input = fs::read_to_string("./test/test.yaml").expect("Couldn't read file");
+    let doc = YamlLoader::load_from_str(&raw_input);
 
-    println!("{doc:?}");
+    match &doc {
+        Err(msg) => println!("Couldn't open config file : {msg}"),
+        Ok(v) => {
+            for yaml in v {
+                handle_config(yaml);
+            }
+        }
+    }
 }

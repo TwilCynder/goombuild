87b06dcfa1aa2061df8906fc980417d81020032e
TwilCynder <twilcynder@gmail.com>
2025-01-28 15:25:15 +0100

    get_as : attempt to further factorize yaml extraction

--- src/config/read.rs
+++ src/config/read.rs
@@ -34,13 +34,6 @@ impl Display for ContentError {
     }
 }
 
-fn handle_wrong_type(yaml_value: &Yaml, expected: &'static str) -> ContentError {
-    match yaml_value {
-        Yaml::BadValue => ContentError::from("Invalid yaml"),
-        _ =>ContentError::wrong_type(expected)
-    }
-}
-
 fn to_unsinged(source: i64) -> Result<u64, ContentError> {
     match source.try_into(){
         Err(_) => Err(ContentError::WrongType("should be a positive integer")),
@@ -48,43 +41,62 @@ fn to_unsinged(source: i64) -> Result<u64, ContentError> {
     }
 }
 
+fn handle_wrong_type(yaml_value: &Yaml, expected: &'static str) -> ContentError {
+    match yaml_value {
+        Yaml::BadValue => ContentError::from("Invalid yaml"),
+        _ =>ContentError::wrong_type(expected)
+    }
+}
+
 fn get_data<'a>(data: &'a Hash, key: &'static str) -> Option<&'a Yaml> {
     data.get(&Yaml::from_str(key))
 }
 
-fn get_int<'a>(data: &'a Hash, key: &'static str) -> Result<Option<i64>, ContentError> {
-    match get_data(data, key) {
-        Some(v) => match v {
-            Yaml::Integer(n) => Ok(Some(*n)),
-            val => Err(handle_wrong_type(val, "integer"))
-        },
+type YamlResult<T> = Result<Option<T>, ContentError>;
+
+trait YamlTypeExtractor <'a, T> {
+    fn extract(yaml: &'a Yaml) -> Result<Option<T>, ContentError>;
+}
+
+fn get_as<'a, T, E: YamlTypeExtractor<'a, T>>(data: &'a Hash, key: &'static str) -> YamlResult<T> {
+    match get_data(data, key){
         None => Ok(None),
+        Some(v) => E::extract(v)
     }
 }
 
-fn get_str <'a>(data: &'a Hash, key: &'static str) -> Result<Option<&'a str>, ContentError>{
-    match data.get(&Yaml::from_str(key)) {
-        None => Ok(None),
-        Some(v) => match v {
-            Yaml::String(str) => Ok(Some(&str.as_str())),
-            val => Err(handle_wrong_type(val, "string"))
+struct StringExtractor; impl<'a> YamlTypeExtractor<'a, &'a str> for StringExtractor {
+    fn extract(yaml: &'a Yaml) -> YamlResult<&'a str> {
+        match yaml {
+            Yaml::String(str) => Ok(Some(str.as_str())),
+            _ => Err(handle_wrong_type(&yaml, "string"))
         }
     }
 }
+fn get_str<'a>(data: &'a Hash, key: &'static str) -> YamlResult<&'a str>{get_as::<'a, &'a str, StringExtractor>(data, key)}
 
-fn get_hash<'a>(data: &'a Hash, key: &'static str) -> Result<Option<&'a Hash>, ContentError>{
-    match data.get(&Yaml::from_str(&key)) {
-        None => Ok(None),
-        Some(v) => match v {
+struct IntExtractor; impl <'a> YamlTypeExtractor<'a, i64> for IntExtractor {
+    fn extract(yaml: &'a Yaml) -> YamlResult<i64> {
+        match yaml {
+            Yaml::Integer(n) => Ok(Some(*n)),
+            val => Err(handle_wrong_type(val, "integer"))
+        }
+    }
+}
+fn get_int<'a>(data: &'a Hash, key: &'static str) -> YamlResult<i64>{get_as::<'a, i64, IntExtractor>(data, key)}
+
+struct HashExtractor; impl <'a> YamlTypeExtractor<'a, &'a Hash> for HashExtractor {
+    fn extract(yaml: &'a Yaml) -> Result<Option<&'a Hash>, ContentError> {
+        match yaml {
             Yaml::Hash(hash) => Ok(Some(hash)),
             val => Err(handle_wrong_type(val, "table"))
         }
     }
 }
+fn get_hash<'a>(data: &'a Hash, key: &'static str) -> YamlResult<&'a Hash>{get_as::<'a, &'a Hash, HashExtractor>(data, key)}
 
 impl <'a> Config<'a> {
-
-    pub fn read(data: &Yaml) -> Result<Config, ContentError> {
+    pub fn read(data: &'a Yaml) -> Result<Config<'a>, ContentError> {
         let mut config = Config::new();
 
         match &data {
@@ -96,8 +108,8 @@ impl <'a> Config<'a> {
                     if let Some(str) = get_str(hash, "dir")? {config.src_dir = str}
                     if let Some(n) = get_int(hash, "depth")? {config.src_depth = to_unsinged(n)?}
                 }
-                if let Some(str) = get_str(data, "obj_dir")? {config.obj_dir = str};
-                if let Some(str) = get_str(data, "kind")? {
+                if let Some(str) = get_str(hash, "obj_dir")? {config.obj_dir = str};
+                if let Some(str) = get_str(hash, "kind")? {
                     match str {
                         "cpp" => {
                             config.compiler = "g++";
@@ -110,9 +122,9 @@ impl <'a> Config<'a> {
                         _ => return Err(ContentError::from("Incorrect kind : must be either c or cpp"))
                     }
                 }   
-                if let Some(str) = get_str(data, "src_ext")? {config.src_ext = str};
-                if let Some(str) = get_str(data, "compiler")? {config.compiler = str};
-                if let Some(str) = get_str(data, "compile_flags")? {config.cflags = str};
+                if let Some(str) = get_str(hash, "src_ext")? {config.src_ext = str};
+                if let Some(str) = get_str(hash, "compiler")? {config.compiler = str};
+                if let Some(str) = get_str(hash, "compile_flags")? {config.cflags = str};
 
 
                 return Ok(config);

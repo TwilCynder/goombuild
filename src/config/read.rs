d2cf9f0a772801452f7f8c40dda90170e60d42cf
TwilCynder <twilcynder@gmail.com>
2025-02-18 18:43:37 +0100

    more context in error messages

--- src/config/read.rs
+++ src/config/read.rs
@@ -4,10 +4,40 @@ use yaml_rust2::yaml::{Array, Hash, Yaml};
 
 use super::{BuildConfig, Config, SourceDir, Target};
 
-#[derive(Debug)]
+pub enum ContextfulErrorType {
+    WrongType(&'static str, &'static str),
+    Other(&'static str)
+}
+
+pub struct ContextfulError {
+    err: ContextfulErrorType,
+    context: String
+}
+
+impl ContextfulError {
+    pub fn _with_context<T: ToString>(err: ContextfulErrorType, context_str: impl Fn() -> T) -> Self {
+        Self{err, context: context_str().to_string()}
+    }
+
+    pub fn wrong_type(expected: &'static str, got: &Yaml) -> Self {
+        Self{err: ContextfulErrorType::WrongType(expected, yaml_type_name(&got)), context: String::new()}
+    }
+
+    pub fn add_context<T: ToString>(mut self, context: T) -> Self {
+        self.context = context.to_string() + " " + self.context.as_str();
+        self
+    }
+}
+
+impl From<&'static str> for ContextfulError {
+    fn from(error: &'static str) -> Self {
+        ContextfulError { err: ContextfulErrorType::Other (error), context: String::new() }
+    }
+}
+
 pub enum ContentError {
     Other(&'static str),
-    WrongType(&'static str)
+    Contextful(ContextfulError)
 }
 
 impl From<&'static str> for ContentError {
@@ -16,9 +46,21 @@ impl From<&'static str> for ContentError {
     }
 }
 
-impl ContentError {
-    fn wrong_type(expected: &'static str) -> Self {
-        ContentError::WrongType(expected)
+impl From<ContextfulError> for ContentError {
+    fn from(value: ContextfulError) -> Self {
+        ContentError::Contextful(value)
+    }
+}
+
+impl Display for ContextfulError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.context)?;
+        match &self.err {
+            ContextfulErrorType::WrongType(expected, got) => {
+                write!(f, "should be a {expected} (got {got})")
+            },
+            ContextfulErrorType::Other(mdsg) => f.write_str(&mdsg),
+        }
     }
 }
 
@@ -26,25 +68,38 @@ impl Display for ContentError {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         match &self {
             ContentError::Other(error) => f.write_str(&error),
-            ContentError::WrongType(expected) => {
-                f.write_str("Should be a ")?;
-                f.write_str(&expected)
+            ContentError::Contextful(err )=> {
+                err.fmt(f)
             }
         }
     }
 }
 
-fn _to_unsinged(source: i64) -> Result<u64, ContentError> {
+fn yaml_type_name(yaml_value: &Yaml) -> &'static str {
+    match yaml_value {
+        Yaml::Real(_) => "real number",
+        Yaml::Integer(_) => "integer",
+        Yaml::String(_) => "string",
+        Yaml::Boolean(_) => "boolean",
+        Yaml::Array(_) => "list (aka array)",
+        Yaml::Hash(_) => "table (aka hash)",
+        Yaml::Alias(_) => "yaml alias",
+        Yaml::Null => "null",
+        Yaml::BadValue => "error value",
+    }
+}
+
+fn _to_unsinged(source: i64) -> Result<u64, ContextfulError> {
     match source.try_into(){
-        Err(_) => Err(ContentError::WrongType("should be a positive integer")),
+        Err(_) => Err("should be a positive integer".into()),
         Ok(n) => Ok(n),
     }
 }
 
-fn handle_wrong_type(yaml_value: &Yaml, expected: &'static str) -> ContentError {
+fn handle_wrong_type(yaml_value: &Yaml, expected: &'static str) -> ContextfulError {
     match yaml_value {
-        Yaml::BadValue => ContentError::from("Invalid yaml"),
-        _ =>ContentError::wrong_type(expected)
+        Yaml::BadValue => " : Invalid YAML value".into(),
+        _ => ContextfulError::wrong_type(expected, yaml_value)
     }
 }
 
@@ -52,9 +107,9 @@ fn get_data<'a>(data: &'a Hash, key: &'static str) -> Option<&'a Yaml> {
     data.get(&Yaml::from_str(key))
 }
 
-type YamlResult<T> = Result<Option<T>, ContentError>;
+type YamlResult<T> = Result<Option<T>, ContextfulError>;
 
-fn try_map_option <T, U, F: Fn(T) -> Result<U, ContentError>> (opt: Option<T>, f: F) -> YamlResult<U> {
+fn try_map_option <T, U, F: Fn(T) -> Result<U, ContextfulError>> (opt: Option<T>, f: F) -> YamlResult<U> {
     Ok(match opt {
         None => None,
         Some(v) => Some(f(v)?)
@@ -64,7 +119,7 @@ fn try_map_option <T, U, F: Fn(T) -> Result<U, ContentError>> (opt: Option<T>, f
 fn get_as<'a, T, F: Fn(&'a Yaml) -> YamlResult<T>>(extract: F, data: &'a Hash, key: &'static str) -> YamlResult<T> {
     match get_data(data, key){
         None => Ok(None),
-        Some(v) => extract(v)
+        Some(v) => extract(v).map_err(|err| err.add_context(key))
     }
 }
 
@@ -93,7 +148,7 @@ fn extract_bool(yaml: & Yaml) -> YamlResult<bool> {
 }
 fn get_bool(data: &Hash, key: &'static str) -> YamlResult<bool>{get_as(extract_bool, data, key)}
 
-fn _extract_hash<'a>(yaml: &'a Yaml) -> Result<Option<&'a Hash>, ContentError> {
+fn _extract_hash<'a>(yaml: &'a Yaml) -> Result<Option<&'a Hash>, ContextfulError> {
     match yaml {
         Yaml::Hash(hash) => Ok(Some(hash)),
         val => Err(handle_wrong_type(val, "table"))
@@ -101,7 +156,7 @@ fn _extract_hash<'a>(yaml: &'a Yaml) -> Result<Option<&'a Hash>, ContentError> {
 }
 fn _get_hash<'a>(data: &'a Hash, key: &'static str) -> YamlResult<&'a Hash>{get_as(_extract_hash, data, key)}
 
-fn extract_array<'a>(yaml: &'a Yaml) -> Result<Option<&'a Array>, ContentError> {
+fn extract_array<'a>(yaml: &'a Yaml) -> Result<Option<&'a Array>, ContextfulError> {
     match yaml {
         Yaml::Array(array) => Ok(Some(array)),
         val => Err(handle_wrong_type(&val, "array"))
@@ -109,7 +164,7 @@ fn extract_array<'a>(yaml: &'a Yaml) -> Result<Option<&'a Array>, ContentError> 
 }
 fn get_array<'a>(data: &'a Hash, key: &'static str) -> YamlResult<&'a Array>{get_as(extract_array, data, key)}
 
-fn array_or_string_into_vec<'a>(yaml: &'a Yaml) -> Result<Vec<&'a str>, ContentError> {
+fn array_or_string_into_vec<'a>(yaml: &'a Yaml) -> Result<Vec<&'a str>, ContextfulError> {
     let mut vec = Vec::<&'a str>::new();
     match yaml {
         Yaml::String(str) => {
@@ -139,11 +194,11 @@ impl <'a> SourceDir <'a> {
         self.dir = str;
     }
 
-    fn read_from_hash(&mut self, hash: &'a Hash) -> Result<(), ContentError>{
+    fn read_from_hash(&mut self, hash: &'a Hash) -> Result<(), ContextfulError>{
         if let Some(str) = get_str(&hash, "dir")?{
             self.dir = str;
         } else {
-            return Err(ContentError::Other("Source directory is missing"))
+            return Err(ContextfulError::from("Source directory is missing").add_context("In source directory"))
         }
         self.ext = get_str(&hash, "ext")?;
         self.depth = get_int(&hash, "depth")?;
@@ -151,19 +206,26 @@ impl <'a> SourceDir <'a> {
         Ok(())
     }
 
-    fn read_from_hash_or_string(yaml: &'a Yaml) -> Result<SourceDir<'a>, ContentError> {
+    fn read_from_hash_or_string(yaml: &'a Yaml) -> Result<SourceDir<'a>, ContextfulError> {
         let mut source = Self::new();
         match yaml {
             Yaml::String(str) => Ok(source.read_from_str(str)),
             Yaml::Hash(hash) => source.read_from_hash(hash),
-            val => Err(handle_wrong_type(val, "string or object"))
+            val => Err(handle_wrong_type(val, "string or object")).into()
         }?;
         Ok(source)
     }
 }
 
+fn ctx<T, C: ToString>(res: Result<T, ContextfulError>, context_l: impl Fn() -> C) -> Result<T, ContextfulError>{
+    match res {
+        Ok(v) => Ok(v),
+        Err(err) => Err(err.add_context(context_l())),
+    }
+}
+
 impl <'a> BuildConfig<'a> {
-    pub fn read(&mut self, data: &'a Hash) -> Result<(), ContentError>{
+    fn read(&mut self, data: &'a Hash) -> Result<(), ContextfulError>{
         self.exec_name = get_str(data, "exec")?;
         self.libs = try_map_option(get_data(data, "libs"), array_or_string_into_vec)?;
         self.ldflags = get_str(data, "link_options")?.or(get_str(data, "ldflags")?);
@@ -178,12 +240,12 @@ impl <'a> Target<'a> {
     pub fn read(data: &'a Hash) -> Result<Target, ContentError> {
         let name = match get_str(data, "name")? {
             Some(str) => str,
-            None => return Err(ContentError::Other("Target needs a name"))
+            None => return Err(ContentError::Other("Targets need a name"))
         };
 
         let mut target = Target::new(name);
 
-        target.config.read(data)?;
+        ctx(target.config.read(data), || "in target ".to_owned() + name)?;
 
         Ok(target)
     }
@@ -241,21 +303,21 @@ impl <'a> Config<'a> {
                 if let Some(str) = get_dir_name(data, "obj_dir")? {config.obj_dir = str};
                 if let Some(str) = get_dir_name(data, "bin_dir")? {config.bin_dir = str};
 
-                config.default_config.read(data)?;
+                ctx(config.default_config.read(data), || "In default config")?;
                 if let Some(array) = get_array(data, "targets")? {
                     for data in array {
                         match data {
                             Yaml::Hash(hash) => {
                                 config.alt_targets.push(Target::read(hash)?);
                             },
-                            val => return Err(handle_wrong_type(val, "table"))
+                            val => return Err(handle_wrong_type(val, "table").add_context("targets property").into())
                         }
                     }
                 }
 
                 return Ok(config);
             },
-            val => Err(handle_wrong_type(val, "property list"))
+            val => Err(handle_wrong_type(val, "property list").add_context("Config file").into())
         }
     }
 }
